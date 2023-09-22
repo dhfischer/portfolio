5747885ab6ce9c9a3530a2fd0c167afbfbb92a06
Dhavi Fischer <dhavi.fischer@gmail.com>
2023-09-22 09:45:05 -0300

    Remove imagens e comentarios desnecessarios

--- portfolioController.js
+++ portfolioController.js
@@ -1,5 +1,4 @@
 exports.getPortfolio = (req, res) => {
-    // Aqui você pode renderizar uma página HTML que exibe suas informações de portfólio e currículo
-    res.render('portfolio'); // Você precisará criar a view 'portfolio.ejs' ou 'portfolio.pug' também
+    res.render('portfolio');
   };
   
